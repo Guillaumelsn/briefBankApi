9c6bfe3fa3b4168cbb4936f0f070fcddf4dc688e
Yasmine Bouhaik <yazlafrite@MacBook-Pro-de-YazLaFrite.local>
2022-03-28 13:56:38 +0200

    model client

--- src/main/java/com/briefBankApi/api/model/Client.java
+++ src/main/java/com/briefBankApi/api/model/Client.java
@@ -1,5 +1,45 @@
 package com.briefBankApi.api.model;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "client")
+
 public class Client {
 
+	 @Id
+	 @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private int id;
+
+	    @Column(name="raisonSociale")
+	    private String raisonSociale;
+
+	    @Column(name="libelleClient")
+	    private String libelleClient;
+
+	    @Column(name="numeroTel")
+	    private String numeroTel;
+
+	    @Column(name="mail")
+	    private String mail;
+	    
+	    @Column(name="adresse")
+	    private String adresse;
+	    
+	    @Column(name="civilite")
+	    private String civilite;
+	    
+	    @Column(name="dateNaissance")
+	    private Date dateNaissance;
+	    
 }
