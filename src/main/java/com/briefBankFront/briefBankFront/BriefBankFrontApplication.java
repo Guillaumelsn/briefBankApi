54cafff5cea6893ad035ed083c63950fe2112d8c
Guillaumelsn <guillaumelesenne@gmail.com>
2022-03-28 11:34:11 +0200

    init project

---
+++ src/main/java/com/briefBankFront/briefBankFront/BriefBankFrontApplication.java
@@ -0,0 +1,13 @@
+package com.briefBankFront.briefBankFront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BriefBankFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BriefBankFrontApplication.class, args);
+	}
+
+}
