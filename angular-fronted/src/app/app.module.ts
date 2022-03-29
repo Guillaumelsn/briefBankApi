aea4fa75841738460ee67828b87ccb944b5df5ec
Yasmine Bouhaik <yasmine.bouhaik@gmail.com>
2022-03-29 16:01:13 +0200

    component client

--- angular-fronted/src/app/app.module.ts
+++ angular-fronted/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientCardComponent } from './client-card/client-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientCardComponent
   ],
   imports: [
     BrowserModule,
